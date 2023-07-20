59986fa332e73ff063810ea3498722a610eed7db
Franz Garcia <franz.garcia.rv@gmail.com>
2023-07-20 16:11:39 -0400

    terms and conditions fix

--- resources/views/Donor/terms.blade.php
+++ resources/views/Donor/terms.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="utf-8">
@@ -25,46 +25,60 @@
 
 <body>
     <div class="container-fluid position-relative d-flex p-0">
-        <!-- Terms and Conditions Start -->
+        <!-- Términos y Condiciones Start -->
         <div class="container-fluid">
             <div class="row h-100 align-items-center justify-content-center" style="min-height: 100vh;">
                 <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
                     <div class="bg-secondary rounded p-4 p-sm-5 my-4 mx-3">
                         <h1>Términos y Condiciones</h1>
                         <div class="bg-secondary rounded h-100 p-4">
-                            <p>Bienvenido/a a nuestro sitio web. Estos son los términos y condiciones que rigen el uso de nuestro sitio web. Al acceder o utilizar este sitio web, usted acepta cumplir con estos términos y condiciones. Si no está de acuerdo con alguna parte de estos términos, no debe utilizar nuestro sitio web.</p>
+                            <p>Bienvenido/a a nuestro sistema de banco de sangre. Estos son los términos y condiciones que rigen el uso de nuestro sistema. Al acceder o utilizar este sistema, aceptas cumplir con estos términos y condiciones. Si no estás de acuerdo con alguna parte de estos términos, no debes utilizar nuestro sistema.</p>
 
-                            <h3>1. Uso del Sitio Web</h3>
-                            <p>El contenido de este sitio web es solo para fines informativos y no constituye asesoramiento legal, médico, financiero o profesional de ninguna índole.</p>
+                            <h3>1. Uso del Sistema</h3>
+                            <p>El contenido de este sistema de banco de sangre es solo para fines informativos y no constituye asesoramiento legal, médico, financiero o profesional de ninguna índole.</p>
 
                             <h3>2. Propiedad Intelectual</h3>
-                            <p>Todos los derechos de propiedad intelectual de este sitio web y su contenido son propiedad nuestra o de nuestros licenciantes y están protegidos por las leyes de derechos de autor y marcas registradas aplicables.</p>
+                            <p>Todos los derechos de propiedad intelectual de este sistema y su contenido son propiedad nuestra o de nuestros licenciantes y están protegidos por las leyes de derechos de autor y marcas registradas aplicables.</p>
 
                             <h3>3. Privacidad</h3>
-                            <p>Nuestra política de privacidad describe cómo recopilamos, utilizamos y protegemos sus datos personales, incluyendo las fotografías que usted nos proporcione. Al utilizar este sitio web, usted acepta nuestra política de privacidad.</p>
+                            <p>Nuestra política de privacidad describe cómo recopilamos, utilizamos y protegemos tus datos personales, incluyendo las fotografías que nos proporcionas. Al utilizar este sistema, aceptas nuestra política de privacidad.</p>
 
                             <h3>4. Uso de Fotografías</h3>
-                            <p>Al subir o proporcionar fotografías a nuestro sitio web, usted concede al BANCO DE SANGRE REGIONAL DE SANTA CRUZ una licencia no exclusiva, transferible, sublicenciable, libre de regalías y mundial para usar, reproducir, modificar, adaptar y mostrar las fotografías con fines relacionados con los servicios proporcionados en este sitio web. Usted declara que tiene los derechos necesarios para otorgar esta licencia y que dichas fotografías no violan los derechos de terceros.</p>
+                            <p>Al subir o proporcionar fotografías a nuestro sistema, otorgas al BANCO DE SANGRE REGIONAL DE SANTA CRUZ una licencia no exclusiva, transferible, sublicenciable, libre de regalías y mundial para usar, reproducir, modificar, adaptar y mostrar las fotografías con fines relacionados con los servicios proporcionados en este sistema. Declaras que tienes los derechos necesarios para otorgar esta licencia y que dichas fotografías no violan los derechos de terceros.</p>
 
                             <h3>5. Enlaces a Terceros</h3>
-                            <p>Nuestro sitio web puede contener enlaces a sitios web de terceros que no son propiedad ni están controlados por nosotros. No tenemos control sobre el contenido, las políticas de privacidad o las prácticas de los sitios web de terceros y no asumimos ninguna responsabilidad por ellos.</p>
+                            <p>Nuestro sistema puede contener enlaces a sitios web de terceros que no son propiedad ni están controlados por nosotros. No tenemos control sobre el contenido, las políticas de privacidad o las prácticas de los sitios web de terceros y no asumimos ninguna responsabilidad por ellos.</p>
 
                             <h3>6. Exclusión de Responsabilidad</h3>
-                            <p>No ofrecemos garantías ni representaciones con respecto a la precisión o integridad del contenido de este sitio web. Usted asume toda la responsabilidad y el riesgo de su uso del sitio web.</p>
+                            <p>No ofrecemos garantías ni representaciones con respecto a la precisión o integridad del contenido de este sistema. Utilizas este sistema bajo tu propio riesgo y responsabilidad.</p>
 
                             <h3>7. Modificaciones</h3>
-                            <p>Nos reservamos el derecho de modificar estos términos y condiciones en cualquier momento. Las modificaciones entrarán en vigor tan pronto como se publiquen en el sitio web. Es su responsabilidad revisar periódicamente los términos y condiciones para estar informado de las actualizaciones.</p>
+                            <p>Nos reservamos el derecho de modificar estos términos y condiciones en cualquier momento. Las modificaciones entrarán en vigor tan pronto como se publiquen en el sistema. Es tu responsabilidad revisar periódicamente los términos y condiciones para estar informado de las actualizaciones.</p>
 
                             <h3>8. Ley Aplicable</h3>
                             <p>Estos términos y condiciones se rigen por las leyes de Bolivia, y cualquier disputa que surja en relación con estos términos y condiciones estará sujeta a la jurisdicción exclusiva de los tribunales de Santa Cruz de la Sierra, Bolivia.</p>
+                            
+                            <h3>9. Uso de Fotografías para Reconocimiento Facial e Identificación</h3>
 
-                            <p>Si tiene alguna pregunta o inquietud sobre estos términos y condiciones, contáctenos en [correo electrónico de contacto].</p>
+                            <p>El sistema de banco de sangre utiliza tecnología de reconocimiento facial e identificación para garantizar la autenticidad y la precisión de la información proporcionada por los usuarios. Al subir fotografías de su rostro y de su carnet de identidad, usted acepta los siguientes términos:</p>
+
+                            <ul>
+                             <li>Las fotografías que usted proporciona serán utilizadas únicamente con fines de reconocimiento facial y verificación de identidad en el contexto del sistema de banco de sangre.</li>
+                             <li>Sus fotografías no serán compartidas, vendidas o divulgadas a terceros sin su consentimiento explícito, a menos que sea requerido por ley o con fines de seguridad y protección.</li>
+                             <li>Nos comprometemos a implementar medidas de seguridad adecuadas para proteger sus fotografías y evitar el acceso no autorizado a las mismas.</li>
+                              <li>Usted declara que tiene los derechos necesarios para proporcionar las fotografías y que no está infringiendo los derechos de propiedad intelectual, privacidad o cualquier otro derecho de terceros.</li>
+                             <li>El sistema no almacenará sus fotografías después de que se complete el proceso de reconocimiento facial e identificación. Sin embargo, es posible que conservemos cierta información derivada de las fotografías con fines estadísticos y de mejora del sistema, siempre garantizando su anonimato.</li>
+                            </ul>
+
+                            <p>Le recomendamos que utilice fotografías claras y de buena calidad para garantizar una adecuada identificación y verificación. También le recordamos que es responsable de asegurarse de que las fotografías proporcionadas sean precisas y reflejen su identidad real.</p>
+
+                            <p>Si tienes alguna pregunta o inquietud sobre estos términos y condiciones, contáctanos a través de soporte@cgsolutions.online .</p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- Terms and Conditions End -->
+        <!-- Términos y Condiciones End -->
     </div>
 
     <!-- Customized Bootstrap Stylesheet -->
